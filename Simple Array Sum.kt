fe1b2fb61505979c37b5b5abe368e2b892e23c8a
Jose Diaz <jose.diaz@isla.local>
2019-09-29 11:12:11 -0400

    Two Challengues

---
+++ Simple Array Sum.kt
@@ -0,0 +1,33 @@
+import java.io.*
+import java.math.*
+import java.text.*
+import java.util.*
+import java.util.regex.*
+
+/*
+ * Complete the simpleArraySum function below.
+ */
+fun simpleArraySum(ar: Array<Int>): Int {
+    var sumarray = 0;
+    var cantidad = 0;
+    for(ar in ar){
+
+    sumarray = sumarray + ar;
+    cantidad++;
+    }
+
+    return sumarray;
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val arCount = scan.nextLine().trim().toInt()
+
+    val ar = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    val result = simpleArraySum(ar)
+
+    println(result)
+}
+
